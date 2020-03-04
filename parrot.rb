59700c144816662186747403df9e3865c0ffc98e
Bernard  Mel <bernard.mamuyac@gmail.com>
2020-03-04 01:47:27 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,9 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot
-  puts "Squawk!"
-end
-
-def parrot (phrase = "Pretty bird!")
-  "Squawk!"
+def parrot(phrase = "Squawk!")
+  puts "#{phrase}"
+  "#{phrase}"
 end
